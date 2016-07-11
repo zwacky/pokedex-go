871f64f5f915044c743e0b9a4b9b223cace44dfa
Simon Wicki <simon.wicki@justwatch.com>
2016-07-11 23:47:27 +0200

    configs

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
-const port = process.env.POKEDEX_PORT || 5555;
+const PORT = process.env.POKEDEX_PORT || 5555;
+const VALIDATION_TOKEN = process.env.POKEDEX_VALIDATION_TOKEN || '';
 
 // body parser middleware
 app.use(bodyParser.urlencoded({
@@ -16,10 +17,18 @@ app.get('/', (req, res) => {
 	res.status(200).send('works! 💪👌💯🙌');
 });
 
-app.post('/incoming-request', (req, res) => {
+app.get('/incoming-request', (req, res) => {
 	res.status(200).send('💩');
+	if (req.query['hub.mode'] === 'subscribe' &&
+			req.query['hub.verify_token'] === VALIDATION_TOKEN) {
+		console.log('validating webhook');
+		res.status(200).send(req.query['hub.challenge']);
+	} else {
+		console.log('failed validation.');
+		res.sendStatus(403);
+	}
 });
 
-app.listen(port, () => {
-    console.log('listening on port', port);
+app.listen(PORT, () => {
+    console.log('listening on port', PORT);
 });
