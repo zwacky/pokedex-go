df1df781ac29d8664ca7922eb402e1d8ada3a437
Simon Wicki <simon.wicki@justwatch.com>
2016-08-08 01:14:43 +0200

    fix more infos

--- src/messaging-manager.js
+++ src/messaging-manager.js
@@ -16,10 +16,9 @@ function receivedMessage(event) {
 	console.log('Received message for user %d and page %d at %d with message:', senderId, recipientId, timeOfMessage);
 	console.log(JSON.stringify(message));
 
-	const messageId = message.mid;
-
-	const messageText = message.text;
-	const messageAttachments = message.attachments;
+	const messageId = message.mid || null;
+	const messageText = message.text || message;
+	const messageAttachments = message.attachments || null;
 
 	if (messageText) {
 		if (messageText.toUpperCase() === 'HI') {
